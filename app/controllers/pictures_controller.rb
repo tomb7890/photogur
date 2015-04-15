bd5527eed63f48ad52bca781b00525bc3864a4fb
Tom Billingsley <tom.billingsley@gmail.com>
2015-04-15 16:49:17 -0400

    Adding feature to delete pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -41,6 +41,12 @@ class PicturesController < ApplicationController
   end
 
 
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
+
 
   private
   def picture_params
