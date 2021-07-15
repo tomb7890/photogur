f20841f6b11ac24910a1551d32e9f74f6891af03
Tom Billingsley <tom.billingsley@gmail.com>
2021-07-15 16:54:41 -0400

    Update seeds with good data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Picture.create!(
-  :title  => "The old church on the coast of White sea",
-  :artist => "Sergey Ershov",
-  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+  :title  => "Assembling the North American B-25 Mitchell at Kansas City, Kansas (USA)",
+  :artist => "Alfred T. Palmer",
+  :url    => "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Alfred_T._Palmer_-_Assembling_the_North_American_B-25_Mitchell_at_Kansas_City%2C_Kansas_%28USA%29.jpg/780px-Alfred_T._Palmer_-_Assembling_the_North_American_B-25_Mitchell_at_Kansas_City%2C_Kansas_%28USA%29.jpg"
 )
 
 Picture.create!(
-  :title  => "Sea Power",
-  :artist => "Stephen Scullion",
-  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+  :title  => "Christiansborg from the Marble Bridge",
+  :artist => "Moahim",
+  :url    => "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/2018_-_Christiansborg_from_the_Marble_Bridge.jpg/800px-2018_-_Christiansborg_from_the_Marble_Bridge.jpg"
 )
 
 Picture.create!(
-  :title  => "Into the Poppies",
-  :artist => "John Wilhelm",
-  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+  :title  => "Mohiniyattam at Kerala School Kalolsavam",
+  :artist => "Shagil Kannur",
+  :url    => "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Mohiniyattam_at_Kerala_School_Kalolsavam_2019_02.jpg/800px-Mohiniyattam_at_Kerala_School_Kalolsavam_2019_02.jpg"
+
 )
