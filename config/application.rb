d8574b2959653b08ce7bd8449c48ae7d5feaa960
Tom Billingsley <tom.billingsley@gmail.com>
2021-07-16 08:03:16 -0400

    Use Rails 5 security features

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module Photogur
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_controller.per_form_csrf_tokens = true
+    config.action_controller.forgery_protection_origin_check = true
+    config.ssl_options = { hsts: { subdomains: true } }
     config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
