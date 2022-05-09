d9d2a7942595c3fdef4ac70e595740d4aa904079
Tom Billingsley <tom.billingsley@gmail.com>
2022-05-09 11:05:59 -0400

    Add version info to migration file

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415190649) do
+ActiveRecord::Schema.define(version: 2015_04_15_190649) do
 
   create_table "pictures", force: :cascade do |t|
-    t.string   "artist"
-    t.string   "title"
-    t.string   "url"
+    t.string "artist"
+    t.string "title"
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
